81ed030f3cc9547cc04ddf2f520ec6598381623e
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-01-09 20:47:37 +0300

    added design dependencies, changed the project structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../resources/logo.svg';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
