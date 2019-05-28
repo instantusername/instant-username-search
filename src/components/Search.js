08b12e14a3355580b533f9df3a838f11d3e51230
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-05-28 23:28:41 +0300

    remove unused import

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import { Input, Icon } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import '../styles/Search.css';
