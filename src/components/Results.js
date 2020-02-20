5dfedebfd27abc6e09cb1f7948339874c9aebb04
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2020-02-20 21:54:37 +0300

    feat(results): implement debounce in results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,22 +1,47 @@
-/* eslint-disable react/prop-types */
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect, useState, useCallback } from 'react';
+import debounce from 'debounce';
 import '../styles/Results.css';
 import ResultCard from './ResultCard';
 
-export default function Results({ username, services, isLoading }) {
-  const cards = useMemo(
+export default function Results({ username, services }) {
+  // these are cards with loading state enabled. Create once, use many times
+  const spinningCards = useMemo(
     () =>
       services.map(service => (
-        <ResultCard
-          username={username}
-          serviceName={service.service}
-          endpoint={`${window.apiUrl}check/${service.service}/${username}/`}
-          key={service.service}
-          spin={isLoading}
-        />
+        <ResultCard serviceName={service.service} key={service.service} spin />
       )),
-    [username, services, isLoading],
+    [services],
   );
 
-  return useMemo(() => <div className="results">{cards}</div>, [cards]);
+  // cards to render in this component
+  const [cards, setCards] = useState(spinningCards);
+
+  // returns real functional cards
+  const createCards = (username, services) =>
+    services.map(service => (
+      <ResultCard username={username} serviceName={service.service} key={service.service} />
+    ));
+
+  // sets the cards with a debounce. This allows us not to calculate real cards while user keeps typing
+  const debouncedSetCards = useCallback(
+    debounce((username, services) => {
+      setCards(createCards(username, services));
+    }, 1000),
+    [],
+  );
+
+  // use spinning cards until user stops typing (debounce)
+  useEffect(() => {
+    setCards(spinningCards);
+    debouncedSetCards(username, services);
+    return () => {
+      debouncedSetCards.clear();
+    };
+    // eslint-disable-next-line
+  }, [username, services]);
+
+  // render cards
+  return useMemo(() => {
+    return <div className="results">{cards}</div>;
+  }, [cards]);
 }
