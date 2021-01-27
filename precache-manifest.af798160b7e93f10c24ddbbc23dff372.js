ccde654fde49bfbaeb203be988e29b96315e32f8
umutcanbolat <umutcanbolat@users.noreply.github.com>
2021-01-27 02:27:07 +0000

    deploy: 23e01b9041cf2b443ec7f5d7cb8ff24c7efc4ee2

--- precache-manifest.af798160b7e93f10c24ddbbc23dff372.js
+++ precache-manifest.af798160b7e93f10c24ddbbc23dff372.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e90e9bc6c82f68266768a07ed96073bd",
+    "revision": "250f367fe458496a7fec8ae7426e9247",
     "url": "/index.html"
   },
   {
-    "revision": "e8e3d22d3815ec20ff55",
+    "revision": "fb9711dd9a560a3406b1",
     "url": "/static/css/2.81187608.chunk.css"
   },
   {
-    "revision": "8629883bde15036296db",
-    "url": "/static/css/main.1c396549.chunk.css"
+    "revision": "e4664a863e5474fedb9e",
+    "url": "/static/css/main.6a46153c.chunk.css"
   },
   {
-    "revision": "e8e3d22d3815ec20ff55",
-    "url": "/static/js/2.f5194c24.chunk.js"
+    "revision": "fb9711dd9a560a3406b1",
+    "url": "/static/js/2.4dc094a4.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.f5194c24.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.4dc094a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8629883bde15036296db",
-    "url": "/static/js/main.08056d02.chunk.js"
+    "revision": "e4664a863e5474fedb9e",
+    "url": "/static/js/main.8448008f.chunk.js"
   },
   {
     "revision": "c1edd6d1f37dbadafb8c",
