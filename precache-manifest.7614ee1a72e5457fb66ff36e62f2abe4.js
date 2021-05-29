50242524d02130f6bf03753e941fc25dbce6bc52
Releaser Astronaut <78322057+ius-release-bot@users.noreply.github.com>
2021-05-29 01:21:44 +0000

    deploy: b97f9dd27d24bda5a8d9fee741304c46a89dcaf8

--- precache-manifest.7614ee1a72e5457fb66ff36e62f2abe4.js
+++ precache-manifest.7614ee1a72e5457fb66ff36e62f2abe4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d7990cbda89b57bafce8c003733a2ca",
+    "revision": "d1cd3a2bd6da0bf31cd1987cb51d908b",
     "url": "/index.html"
   },
   {
-    "revision": "f8e7d8b9a6de5721a4a5",
+    "revision": "fb9711dd9a560a3406b1",
     "url": "/static/css/2.81187608.chunk.css"
   },
   {
-    "revision": "b20606e083059331f10c",
+    "revision": "f985b9d317d53e3484ae",
     "url": "/static/css/main.6a46153c.chunk.css"
   },
   {
-    "revision": "f8e7d8b9a6de5721a4a5",
-    "url": "/static/js/2.b1c7fd6a.chunk.js"
+    "revision": "fb9711dd9a560a3406b1",
+    "url": "/static/js/2.4dc094a4.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.b1c7fd6a.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.4dc094a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b20606e083059331f10c",
-    "url": "/static/js/main.6452998d.chunk.js"
+    "revision": "f985b9d317d53e3484ae",
+    "url": "/static/js/main.1c68f4c3.chunk.js"
   },
   {
     "revision": "c1edd6d1f37dbadafb8c",
