02b025dba9eaba8b1777b8e5be337687dca4abbb
Emre Çakır <ceamkrier@gmail.com>
2021-06-12 01:13:01 +0300

    feat: 🎸 newsletter registry logic implemented

--- src/components/NewsletterSubscription.js
+++ src/components/NewsletterSubscription.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Input } from 'antd';
 
 import '../styles/NewsletterSubscription.css';
 
 export default function NewsletterSubscription({ illustrationEnabled = false }) {
+  const [subscriptionInfo, setSubscriptionInfo] = useState(null);
+
+  const handleNewsletterSubscription = useCallback(async email => {
+    // send request to subscriptionHandler
+    const subscriptionStatus = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ status: true, message: 'subscribed' });
+      }, 1500);
+
+      // case: email address already subscribed
+      // setTimeout(() => {
+      //   resolve({ status: true, message: 'existing_subscription' });
+      // }, 1500);
+    }).catch(err => {
+      return { status: false, message: 'server_exception' };
+    });
+
+    setSubscriptionInfo(subscriptionStatus);
+
+    setTimeout(() => {
+      setSubscriptionInfo(null);
+    }, 2000);
+  }, []);
+
   return (
     <div className="newsletterSubscription-container">
       {illustrationEnabled && (
@@ -20,7 +44,18 @@ export default function NewsletterSubscription({ illustrationEnabled = false }) 
         </p>
       </div>
       <div className="newsletterSubscription-input">
-        <Input.Search placeholder="Your mail address" enterButton="Subscribe"></Input.Search>
+        <Input.Search
+          placeholder="Your mail address"
+          enterButton="Subscribe"
+          onSearch={handleNewsletterSubscription}
+        ></Input.Search>
+        <span
+          className={`newsletterSubscription-flashMessage ${
+            subscriptionInfo ? 'messageReceived' : ''
+          }`}
+        >
+          {(subscriptionInfo && subscriptionInfo.message) || 'Placeholder'}
+        </span>
       </div>
     </div>
   );
