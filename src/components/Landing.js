21fafcde1496bfb635e415e03dca7ca3a023813c
Emre Çakır <ceamkrier@gmail.com>
2021-06-15 20:28:48 +0300

    feat: 🎸 bare-minimum of translations defined

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,6 @@ import { FormattedMessage } from 'react-intl';
 
 import WhyUs from './WhyUs';
 import NewsletterSubscription from './NewsletterSubscription';
-import FrequentQuestions from './FrequentQuestions';
 import Donations from './Donation';
 
 import '../styles/Landing.css';
@@ -11,55 +10,80 @@ import '../styles/Landing.css';
 export default function Landing() {
   const whyUsHeader = {
     enabled: true,
-    headerText: 'Find your next username',
-    subText: 'Expand your brand across the web',
+    headerText: <FormattedMessage id="whyUs.header.header" default="Find your next username" />,
+    subText: (
+      <FormattedMessage id="whyUs.header.sub" defaultMessage="Expand your brand across the web" />
+    ),
   };
 
   const whyUsRows = [
     {
       id: 'benefits',
-      header: 'A new idea? We got your back!',
-      details: 'You can easily test & find the best suitable username for your next endevour',
+      header: (
+        <FormattedMessage
+          id="whyUs.row.benefit.header"
+          defaultMessage="A new idea? We got your back!"
+        />
+      ),
+      details: (
+        <FormattedMessage
+          id="whyUs.row.benefit.details"
+          defaultMessage="You can easily test & find the best suitable username for your next endevour"
+        />
+      ),
       image: require('../resources/idea.svg'),
     },
     {
       id: 'platforms',
-      header: 'One click to see them all',
-      details:
-        'You can easily search on 100+ platforms for your username. Also, you can use advanced search to narrow down your results',
+      header: (
+        <FormattedMessage
+          id="whyUs.row.platforms.header"
+          defaultMessage="One click to see them all"
+        />
+      ),
+      details: (
+        <FormattedMessage
+          id="whyUs.row.platforms.details"
+          defaultMessage="You can easily search on 100+ platforms for your username. Also, you can use advanced search to narrow down your results"
+        />
+      ),
       image: require('../resources/search.svg'),
     },
     {
       id: 'instant',
-      header: 'Ready to start?',
-      details:
-        'So, you found your new username and would love to register them. If you like, we can automate it for you! (Coming soon)',
+      header: <FormattedMessage id="whyUs.row.instant.header" defaultMessage="Ready to start?" />,
+      details: (
+        <FormattedMessage
+          id="whyUs.row.instant.details"
+          defaultMessage="So, you found your new username and would love to register them. If you like, we can automate it for you! (Coming soon)"
+        />
+      ),
       image: require('../resources/servant.svg'),
     },
   ];
 
-  const frequentlyAskedQuestions = [
-    {
-      question: 'Some question',
-      answer:
-        'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
-    },
-    {
-      question: 'Another question',
-      answer:
-        'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
-    },
-    {
-      question: 'Another question with another answer',
-      answer:
-        'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
-    },
-    {
-      question: 'Another question with different answer',
-      answer:
-        'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
-    },
-  ];
+  // const frequentlyAskedQuestions = [
+  //   {
+  //     question: 'Some question',
+  //     answer:
+  //       'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
+  //   },
+  //   {
+  //     question: 'Another question',
+  //     answer:
+  //       'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
+  //   },
+  //   {
+  //     question: 'Another question with another answer',
+  //     answer:
+  //       'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
+  //   },
+  //   {
+  //     question: 'Another question with different answer',
+  //     answer:
+  //       'Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.',
+  //   },
+  // ];
 
   return (
     <>
