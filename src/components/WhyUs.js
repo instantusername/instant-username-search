10371b620930b41c9870548bf84c803e2661a96e
umutcanbolat <hello@umutcanbolat.com>
2021-06-17 19:11:53 +0200

    feat: 🎸 style whyus more

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -12,19 +12,17 @@ export default function WhyUs({ headerConfig, rows }) {
         </div>
       )}
 
-      {rows.map((r, i) => {
-        return (
-          <div key={r.id} className={`whyus-row ${(i + 1) % 2 === 0 && 'reverse-row'}`}>
-            <div className="whyus-column">
-              <img src={r.image} alt={r.header} />
-            </div>
-            <div className="whyus-column">
-              <h3>{r.header}</h3>
-              <p>{r.details}</p>
-            </div>
+      {rows.map((r, i) => (
+        <div key={r.id} className={i % 2 === 0 ? 'whyus-row' : 'whyus-reverse-row'}>
+          <div className="whyus-column">
+            <img src={r.image} alt={r.header} />
           </div>
-        );
-      })}
+          <div className="whyus-column">
+            <h3>{r.header}</h3>
+            <p>{r.details}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
