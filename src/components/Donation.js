8ce68c243abe7b0e045297b00084bb8d171faba1
umutcanbolat <hello@umutcanbolat.com>
2021-06-20 22:11:41 +0200

    feat: 🎸 style donations

--- src/components/Donation.js
+++ src/components/Donation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'antd';
 import { FormattedMessage } from 'react-intl';
 
 import '../styles/Donation.css';
@@ -7,7 +8,7 @@ export default function Donation() {
   return (
     <div className="donation-container">
       <div className="donation-illustration">
-        <img src={require('../resources/thumbs-up.png')} alt="Donation illustration" />
+        <img src={require('../resources/astronaut.svg')} alt="Donation illustration" />
       </div>
       <div className="donation-body">
         <h2>Enjoying the service?</h2>
@@ -17,30 +18,32 @@ export default function Donation() {
           this service and provide it to everyone. If you find our service helpful, consider
           supporting us!
         </p>
-        <div className="donation-optionsRow">
-          <div className="donation-option">
-            <div className="bmc">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.buymeacoffee.com/1ulP4IGFm"
-              >
-                <img
-                  src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg"
-                  alt="Buy me a coffee!"
-                />
-                <span>
-                  <FormattedMessage id="app.coffee" defaultMessage="Buy me a coffee" />
-                </span>
-              </a>
-            </div>
-          </div>
-          <div className="donation-option">
-            <div className="cryptocurrency">
+      </div>
+      <div className="donation-optionsRow">
+        <div className="donation-option">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.buymeacoffee.com/1ulP4IGFm"
+          >
+            <Card className="donation-card" hoverable>
+              <img
+                src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg"
+                alt="Buy me a coffee!"
+              />
+              <span>
+                <FormattedMessage id="app.coffee" defaultMessage="Buy me a coffee" />
+              </span>
+            </Card>
+          </a>
+        </div>
+        <div className="donation-option">
+          <a target="_blank" rel="noopener noreferrer" href="#">
+            <Card className="donation-card" hoverable>
               <img src={require('../resources/bitcoin.svg')} alt="Buy me a coffee!" />
-              Donate cryptocurrency <br></br> (Coming soon)
-            </div>
-          </div>
+              <span>Donate cryptocurrency</span>
+            </Card>
+          </a>
         </div>
       </div>
     </div>
