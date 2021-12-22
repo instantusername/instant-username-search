8617eafcd1913702786b1d82cb2202e4ac7b8f35
umutcanbolat <hello@umutcanbolat.com>
2021-12-22 07:33:12 +0100

    feat: 🎸 memoize Results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import debounce from 'debounce';
 import SortableResultCard from './SortableResultCard';
 import '../styles/Results.css';
@@ -16,23 +16,29 @@ export default function Results({ username, services, setServices }) {
   useEffect(() => {
     setEffectiveUsername('');
     setEffectiveUsernameDebounced(username);
+
+    // clear the debounced function on cleanup
+    return setEffectiveUsernameDebounced.clear;
   }, [username, setEffectiveUsernameDebounced]);
 
-  return (
-    <>
-      <div className="results">
-        {services.map(({ service, endpoint }) => {
-          const endpointWithUsername = endpoint.replace('{username}', effectiveUsername);
-          return (
-            <SortableResultCard
-              key={service}
-              serviceName={service}
-              checkEndpoint={endpointWithUsername}
-              ready={effectiveUsername.length > 0}
-            />
-          );
-        })}
-      </div>
-    </>
+  return useMemo(
+    () => (
+      <>
+        <div className="results">
+          {services.map(({ service, endpoint }) => {
+            const endpointWithUsername = endpoint.replace('{username}', effectiveUsername);
+            return (
+              <SortableResultCard
+                key={service}
+                serviceName={service}
+                checkEndpoint={endpointWithUsername}
+                ready={effectiveUsername.length > 0}
+              />
+            );
+          })}
+        </div>
+      </>
+    ),
+    [effectiveUsername, services],
   );
 }
