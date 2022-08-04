6f26bc23a7bdc9ad6a03eb99cec19095d179109c
umutcanbolat <hello@umutcanbolat.com>
2022-08-04 23:03:44 +0200

    refactor: 💡 refactor search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,44 +1,39 @@
-import React, { useMemo } from 'react';
+import React, { useState } from 'react';
 import { Input, Icon } from 'antd';
 import { Link } from 'react-router-dom';
-// TODO: use styled components instead
+
 import '../styles/Search.css';
 
-export default function Search({ input, onChange }) {
-  const inputChanged = ({ target }) => {
-    // niceInput is the url friendly version of the input
-    let niceInput = target.value.replace(/[^a-zA-Z0-9-_.]/g, '');
-    onChange(niceInput);
+export default function Search({ onChange }) {
+  const [input, setInput] = useState('');
+
+  const handleChange = text => {
+    let urlFriendlyInput = text.replace(/[^a-zA-Z0-9-_.]/g, '');
+    setInput(urlFriendlyInput);
+    onChange(urlFriendlyInput);
   };
 
-  const clearInput = () => {
-    onChange('');
-  };
-
-  return useMemo(() => {
-    return (
-      <div className="search">
-        <Link
-          to={'/'}
-          onClick={() => {
-            clearInput();
-          }}
-        >
-          <div className="header">
-            <Icon type="thunderbolt" theme="filled" />
-            <h1>{'Instant Username Search'}</h1>
-          </div>
-        </Link>
-        <Input
-          placeholder={'Search username'}
-          size="large"
-          allowClear
-          value={input}
-          onChange={inputChanged}
-          autoFocus
-        />
-      </div>
-    );
-    // eslint-disable-next-line
-  }, [input]);
+  return (
+    <div className="search">
+      <Link
+        to={'/'}
+        onClick={() => {
+          handleChange('');
+        }}
+      >
+        <div className="header">
+          <Icon type="thunderbolt" theme="filled" />
+          <h1>Instant Username Search</h1>
+        </div>
+      </Link>
+      <Input
+        placeholder={'Search username'}
+        size="large"
+        allowClear
+        value={input}
+        onChange={event => handleChange(event.target.value)}
+        autoFocus
+      />
+    </div>
+  );
 }
