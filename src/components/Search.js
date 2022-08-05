8de668b3c9ec3223789f3b013ce6137123aff172
umutcanbolat <hello@umutcanbolat.com>
2022-08-05 18:58:12 +0200

    feat: handle appState in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Input, Icon } from 'antd';
 import { Link } from 'react-router-dom';
+import isEmpty from 'lodash/isEmpty';
+import debounce from 'debounce';
 
 import '../styles/Search.css';
 
-export default function Search({ onChange }) {
+export default function Search({ onInputChange, onStateChange }) {
   const [input, setInput] = useState('');
 
+  const debouncedSearchHandler = useCallback(
+    debounce(input => {
+      onInputChange(input);
+      onStateChange('search');
+    }, 800),
+    [],
+  );
+
   const handleChange = text => {
+    if (isEmpty(text)) {
+      debouncedSearchHandler.clear();
+      setInput('');
+      onInputChange('');
+      onStateChange('empty');
+      return;
+    }
+    onStateChange('userTyping');
+
     let urlFriendlyInput = text.replace(/[^a-zA-Z0-9-_.]/g, '');
     setInput(urlFriendlyInput);
-    onChange(urlFriendlyInput);
+    debouncedSearchHandler(urlFriendlyInput);
   };
 
   return (
